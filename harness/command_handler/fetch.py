2d103903a2adde162574c70b90e610e4aa8b8f38
Jim Hawkins <hawkinsji@vmware.com>
2022-12-03 19:38:36 +0000

    Tidy up the code, remove commented out chunks and unused imports
    Signed-off-by: Jim Hawkins <hawkinsji@vmware.com>

--- harness/command_handler/fetch.py
+++ harness/command_handler/fetch.py
@@ -20,11 +20,12 @@ import base64
 # Cryptographic hash module. Only used to generate a certificate thumbprint.
 # https://docs.python.org/3/library/hashlib.html
 import hashlib
-
-
+#
+# HTTP modules.
+# https://docs.python.org/3/library/http.client.html#http.client.HTTPSConnection
 from http.client import HTTPSConnection
-
-
+# Also for reference but without explicit imports.
+# https://docs.python.org/3/library/http.client.html#httpresponse-objects
 #
 # JSON module.
 # https://docs.python.org/3/library/json.html
@@ -56,13 +57,9 @@ import sys
 # https://docs.python.org/3/library/tempfile.html
 from tempfile import NamedTemporaryFile
 #
-# Module for HTTP errors.
-# https://docs.python.org/3/library/urllib.error.html
-import urllib.error
-#
-# Module for URL requests.
-# https://docs.python.org/3/library/urllib.request.html
-import urllib.request
+# URL parsing module.
+# https://docs.python.org/3/library/urllib.parse.html#urllib.parse.urlparse
+from urllib.parse import urlparse
 #
 # Local Imports.
 #
@@ -148,13 +145,16 @@ class Fetcher:
 #
 # Returns a 400 and error message in HTML.
 # https://client.badssl.com/
-
+#
+# Returns a 404 and the usual front page HTML.
+# https://example.com/bobo
 
     def fetch(self, parameters, httpHandler=None):
         peerCertEncoded = None
         peerCertLength = None
         fetchedRaw = None
 
+        # Embedded function to construct the somewhat complex return object.
         def return_(status, fetched, details):
             return_ = {
                 "peerCertificateDER": peerCertEncoded,
@@ -172,37 +172,11 @@ class Fetcher:
                     return_['fetchedDetails']['status'] = status
             return return_
 
-        # resource = parameters['resource']
-        # url = urllib.parse.urlparse(resource)
-        # host = url.hostname
-        # port = 443 if url.port is None else url.port
         url, port, fetchError = self._parse_resource(parameters)
         self._log(httpHandler, f'fetch() {url.hostname} {port}.')
         if fetchError is not None:
             return return_(0, None, fetchError)
 
-        # if url.hostname is None:
-        #     return return_(1, None, {
-        #         'statusText': "No url.hostname in parameters.resource",
-        #         'resource': resource,
-        #         'url': f'{url}'
-        #     })
-
-
-
-        # try:
-        #     connection = HTTPSConnection(
-        #         url.hostname, port=port, context=self._sslContext)
-        # except Exception as error:
-        #     return return_(2, None, {
-        #         'statusText': f'HTTPSConnection({url.hostname},{port},) {error}'})
-
-        # try:
-        #     connection.connect()
-        # except Exception as error:
-        #     return return_(3, None, {
-        #         'statusText': (
-        #             f'HTTPSConnection({url.hostname},{port},).connect() {error}')})
         connection, fetchError = self._connect(url.hostname, port)
         if fetchError is not None:
             return return_(1, None, fetchError)
@@ -215,10 +189,6 @@ class Fetcher:
 
         if details['status'] >= 400:
             return return_(None, None, details)
-
-        # fetched = self.fetch_JSON(parameters, httpHandler, host, port)
-        # peerCertEncoded, peerCertLength = self.get_peer_certificate(
-        #     host, port, httpHandler)
 
         fetchedObject, fetchError = self._parse_JSON(fetchedRaw)
         if fetchError is not None:
@@ -235,33 +205,20 @@ class Fetcher:
         
         return return_(None, fetchedObject, details)
 
-        # return {
-        #     **fetched,
-        #     "peerCertificateDER": peerCertEncoded,
-        #     "peerCertificateLength": peerCertLength
-        # }
-
     def _parse_resource(self, parameters):
         try:
             resource = parameters['resource']
         except KeyError:
             return None, None, {
                 'statusText': 'No "resource" in parameters.',
-                'parameterKeys': tuple(parameters.keys())
-            }
+                'parameterKeys': tuple(parameters.keys())}
 
-        url = urllib.parse.urlparse(resource)
-        # host = url.hostname
-        # port = 443 if url.port is None else url.port
-        # if url.port is None:
-        #     url.port = 443
-
+        url = urlparse(resource)
         if url.hostname is None:
             return None, None, {
                 'statusText': "No host in parameters.resource",
                 'resource': resource,
-                'url': f'{url}'
-            }
+                'url': f'{url}'}
 
         return url, 443 if url.port is None else url.port, None
 
@@ -326,13 +283,6 @@ class Fetcher:
         except KeyError:
             pass
 
-            # if 'body' in options:
-            #     body = options['body'].encode()
-            # elif 'bodyObject' in options:
-            #     body = json.dumps(options['bodyObject']).encode()
-            # else:
-            #     body = None
-
         connection.endheaders(
             options['body'].encode() if 'body' in options else
             json.dumps(options['bodyObject']).encode()
@@ -341,8 +291,8 @@ class Fetcher:
         connection.send(b'')
         response = connection.getresponse()
 
-        # https://docs.python.org/3/library/http.client.html#httpresponse-objects
-
+        # ToDo: Make it handle different encoding schemes instead of assuming
+        # utf-8.
         return response.read().decode('utf-8'), {
             'status': response.status,
             'statusText': response.reason,
@@ -355,7 +305,6 @@ class Fetcher:
 
         try:
             return json.loads(raw), None
-
         except json.decoder.JSONDecodeError as error:
             # https://docs.python.org/3/library/json.html#json.JSONDecodeError
             return None, {
@@ -363,104 +312,8 @@ class Fetcher:
                 'headers': {
                     'msg':error.msg,
                     'lineno': error.lineno, 'colno': error.colno
-                }
-            }
+                }}
 
-    def fetch_JSON(self, parameters, httpHandler, host, port):
-        # Fetch the resource in the context. ToDo:
-        #
-        # -   Make it handle different encoding schemes instead of assuming
-        #     utf-8.
-
-        request = urllib.request.Request(parameters['resource'])
-
-        if 'options' in parameters:
-            options = parameters['options']
-            if 'method' in options:
-                request.method = options['method']
-            if 'body' in options:
-                request.data = options['body'].encode()
-                # Assume it's JSON.
-                request.add_header('Content-Type', "application/json")
-            if 'bodyObject' in options:
-                request.data = json.dumps(options['bodyObject']).encode()
-                request.add_header('Content-Type', "application/json")
-            if 'headers' in options:
-                for header, value in options['headers'].items():
-                    request.add_header(header, value)
-
-        self._log(
-            httpHandler,
-            f'Request:\nhost "{request.host}"\nBody: {request.data}'
-            f'\nselector "{request.selector}"'
-            f'\nheaders {request.header_items()}'
-        )
-
-        opened = None
-        details = None
-        try:
-            # opened = urllib.request.urlopen(request, context=self._sslContext)
-            connection = HTTPSConnection(
-                host, port=port, context=self._sslContext
-            )
-            self._log(httpHandler, f'{connection.sock} {dir(connection)}')
-
-            connection.connect()
-            self._log(httpHandler, f'{connection.sock} {dir(connection)}')
-
-            sslSocket = connection.sock
-            peerCertBinary = sslSocket.getpeercert(True)
-            peerCertDict = sslSocket.getpeercert(False)
-            self._log(httpHandler, f'{peerCertDict}')
-
-
-
-            opened = None
-
-            connection.close()
-            details = {
-                # 'opened': True,
-                # 'status': opened.status,
-                # 'statusText': opened.msg,
-                # 'headers': dict(opened.headers.items())
-            }
-        except urllib.error.HTTPError as error:
-            details = {
-                'opened': False,
-                'status': error.code,
-                'statusText': error.reason,
-                'headers': dict(error.headers.items())
-            }
-        self._log(httpHandler, f'Opened details: {details}')
-
-        raw = None if opened is None else opened.read().decode('utf-8')
-        fetchedLength = None if raw is None else len(raw)
-        self._log(httpHandler, f'Fetched length: {fetchedLength}.')
-
-        if details['status'] >= 400:
-            return {'fetchError': details, 'fetchedRaw': raw}
-
-
-
-        try:
-            return {
-                'fetched': (
-                    None if raw is None or len(raw) == 0 else json.loads(raw)),
-                'fetchedDetails': details,
-                'fetchedRaw': raw
-            }
-        except json.decoder.JSONDecodeError as error:
-            return {
-                #f'JSONDecodeError {error}',
-                # https://docs.python.org/3/library/json.html#json.JSONDecodeError
-                'fetchError': {
-                    'status': 0,
-                    'statusText': error.msg,
-                    'headers': { 'lineno': error.lineno, 'colno': error.colno }
-                },
-                'fetchedRaw': raw
-            }
-    
     def openssl_thumbprint(self, serverName, connectAddress, httpHandler):
         # TOTH Generate fingerprint with openssl and Python:
         # https://stackoverflow.com/q/70781380/7657675
