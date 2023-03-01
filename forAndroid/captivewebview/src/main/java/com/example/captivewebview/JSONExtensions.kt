47e158905851ab12c0fa5e7c6a22411d236168ea
Jim Hawkins <hawkinsji@vmware.com>
2023-03-01 08:47:02 +0000

    Add documentation in a long comment
    Signed-off-by: Jim Hawkins <hawkinsji@vmware.com>

--- forAndroid/captivewebview/src/main/java/com/example/captivewebview/JSONExtensions.kt
+++ forAndroid/captivewebview/src/main/java/com/example/captivewebview/JSONExtensions.kt
@@ -5,6 +5,23 @@ package com.example.captivewebview
 
 import org.json.JSONObject
 
+// The extensions in this file make the members of an enum class usable as
+// JSONObject keys and as String values in the left sides of `to` mappings. They
+// can be imported individually.
+//
+// Code that attempts to use an enum member as a JSONObject key without
+// importing will generate a build-time error, which is good. Fix by adding
+// individual import statements like these.
+//
+//     import com.example.captivewebview.opt
+//     import com.example.captivewebview.put
+//
+// Code that attempts to use an enum member as the String left side of a `to`
+// mapping might only generate a run-time error, which is difficult to
+// troubleshoot. Fix by adding an individual import statement like this.
+//
+//     import com.example.captivewebview.to
+
 inline fun <reified T: Enum<T>> JSONObject.opt(key: T)
 : Any? = opt(key.name)
 
