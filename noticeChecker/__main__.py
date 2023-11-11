5731be9d2f717104ca24e0a22b446ecc089c15e8
Jim Hawkins <hawkinsji@vmware.com>
2023-11-11 18:09:00 +0000

    Add date correction edit with user confirmation, and improve the summary to print a path if it's the only one, and add a summary of copyright suffixes
    Signed-off-by: Jim Hawkins <hawkinsji@vmware.com>

--- noticeChecker/__main__.py
+++ noticeChecker/__main__.py
@@ -3,7 +3,25 @@
 # Run with Python 3
 """Script to check copyright notices.
 
-WiP"""
+1.  Scans the current directory for files under revision control by running git
+    ls-files.
+2.  Runs git log to determine the last modified date for each file.
+3.  Reads each file to discover any embedded copyright notice.
+
+Each file then has one of these states.
+
+-   MISSING, if the file doesn't have a copyright notice.
+-   INCORRECT_DATE, if the year in the copyright doesn't match the git modified
+    date.
+-   CORRECT, otherwise.
+
+The script will, with user confirmation, edit in a corrected date to each file
+with INCORRECT_DATE status.
+
+A summary of file statuses is printed.
+
+Some file formats and names are exempt, such as binary formats and the
+gradlew.bat file."""
 # This file makes the directory a runnable module. To get the command line
 # usage, run it like this.
 #
@@ -40,22 +58,34 @@ def main(commandLine):
         formatter_class=argparse.RawDescriptionHelpFormatter,
         description=textwrap.dedent(__doc__))
     argumentParser.add_argument(
-        '-v', '--verbose', action='store_true', help=
-        "List every file.")
+        '-e', '--edit', choices=['yes', 'y', 'no', 'n', 'prompt']
+        , default='prompt', help=
+        "yes or y to edit without user confirmation; no or n to edit nothing."
+        " Default is to prompt for each file.")
+    argumentParser.add_argument(
+        '-s', '--summarise-first', dest='summariseFirst', action='store_true'
+        , help=
+        "Finish the scan and print the summary before offering any edits."
+        " Default is to offer edits as soon as editable files are scanned.")
     argumentParser.add_argument(
         '--stop-after', dest='stopAfter', type=int, default=0, help=
         'Stop after checking the specified number of files. Or specify zero'
         ' not to stop. This is a diagnostic option. Default is zero.')
     argumentParser.add_argument(
         '--exempt-names', dest='exemptNames', default=exemptNames
-        , metavar='.SUFFIX', type=str, help=
+        , metavar='NAME.SUF', type=str, nargs='*', help=
         'Files with any of these names are exempt from copyright checking.'
         f' Default is {exemptNames}.')
     argumentParser.add_argument(
         '--exempt-suffixes', dest='exemptSuffixes', default=exemptSuffixes
-        , metavar='.SUFFIX', type=str, help=
+        , metavar='.SUFFIX', type=str, nargs='*', help=
         'Files with any of these suffixes are exempt from copyright checking.'
         f' Default is {exemptSuffixes}.')
+    argumentParser.add_argument(
+        '-v', '--verbose', action='store_true', help=
+        "Print every file path and its notice state during the scan. Default is"
+        " to print only a single state indicator character per file during the"
+        " scan.")
     return argumentParser.parse_args(commandLine[1:], NoticeChecker())()
 
 exit(main(argv))
--- noticeChecker/notice_checker.py
+++ noticeChecker/notice_checker.py
@@ -9,9 +9,9 @@
 #
 # Standard library imports, in alphabetic order.
 #
-# Module for file and directory handling.
-# https://docs.python.org/3.5/library/shutil.html
-import shutil
+# Enumeration class module.
+# https://docs.python.org/3/library/enum.html
+import enum
 #
 # Temporary file module.
 # https://docs.python.org/3/library/tempfile.html
@@ -20,15 +20,34 @@ from tempfile import NamedTemporaryFile
 # Local imports.
 #
 from noticeChecker.git_cli import git_ls_files
-from noticeChecker.noticed_file import NoticeState, NoticedFile
+from noticeChecker.noticed_file import NoticeState, NoticedFile, str_quote
+from noticeChecker.overwrite import Overwrite
 
 # General TOTH
 # https://github.com/vmware-samples/workspace-ONE-SDK-integration-samples/blob/main/IntegrationGuideForAndroid/Apps/samers.py
+
+class Edit(enum.Enum):
+    YES = True
+    NO = False
+    PROMPT = None
+
+def first_or_len(paths):
+    return str(paths[0]) if len(paths) == 1 else (str(len(paths)) + ".")
 
 class NoticeChecker:
 
     # Properties that are set by the CLI.
     #
+    @property
+    def edit(self):
+        return self._edit
+    @edit.setter
+    def edit(self, edit):
+        for choice in Edit:
+            if edit[0].lower() == choice.name[0].lower():
+                self._edit = choice
+                break
+
     @property
     def exemptNames(self):
         return self._exemptNames
@@ -51,6 +70,13 @@ class NoticeChecker:
         self._stopAfter = stopAfter
 
     @property
+    def summariseFirst(self):
+        return self._summariseFirst
+    @summariseFirst.setter
+    def summariseFirst(self, summariseFirst):
+        self._summariseFirst = summariseFirst
+
+    @property
     def verbose(self):
         return self._verbose
     @verbose.setter
@@ -60,61 +86,106 @@ class NoticeChecker:
     # End of CLI properties.
 
     def __init__(self):
-        self._suffixes = {}
+        self._noticedFiles = []
 
     def __call__(self):
+        self._overwrite = Overwrite(self.edit.value)
+        self.__scan_files()
+        if self.__print_errors() > 0: return 1
+        self.__print_summary()
+        if self.summariseFirst: self.__correct_dates()
+        return 0
+    
+    def __scan_files(self):
         if not self.verbose:
+            print("Scan dots:")
             for state in NoticeState:
                 print(state.value, state.name)
         stopCount = 0
         for path in git_ls_files():
             stopCount += 1
-            suffix = path.name if path.suffix == "" else path.suffix
-            try:
-                noticedFile = (
-                    NoticedFile.from_exempt_path(path)
-                    if (
-                        suffix in self.exemptSuffixes
-                        or path.name in self.exemptNames
-                    ) else NoticedFile.from_path(path)
-                )
-            except UnicodeDecodeError:
-                print(path)
-                print(
-                    f"Raised UnicodeDecodeError. Should the suffix {suffix}"
-                    " be an exempt binary format?")
-                return 1
-            except Exception as exception:
-                raise RuntimeError(path) from exception
+            noticed = self.__scan_one_file(path)
+            if self.verbose: print(noticed)
+            else: print(noticed.state.value, end='', flush=True)
+            self._noticedFiles.append(noticed)
 
-            if self.verbose:
-                print(noticedFile)
-            else:
-                print(
-                    noticedFile.state.value, end='', flush=True)
-            self.__record_state(
-                path.name if path.name in self.exemptNames else suffix
-                , noticedFile.state)
+            if self.stopAfter > 0 and stopCount >= self.stopAfter: break
 
-            if self.stopAfter > 0 and stopCount >= self.stopAfter:
-                break
-
-        if not self.verbose:
-            print()
-        self.__print_states()
-        print(f"Path count:{stopCount}.")
+        if not self.verbose: print()
+        print(f"Path count: {stopCount}.")
     
-    def __record_state(self, suffix, noticeState):
-        if suffix not in self._suffixes:
-            self._suffixes[suffix] = {}
-        if noticeState.name not in self._suffixes[suffix]:
-            self._suffixes[suffix][noticeState.name] = 0
-        self._suffixes[suffix][noticeState.name] += 1
+    def __scan_one_file(self, path):
+        if (
+            path.suffix in self.exemptSuffixes or path.name in self.exemptNames
+        ): return NoticedFile.from_exempt_path(path)
 
-    def __print_states(self):
+        while True:
+            noticed = NoticedFile.from_path(path)
+            if (not(
+                noticed.exception is None
+                or type(noticed.exception) is UnicodeDecodeError
+            )): raise RuntimeError(path) from noticed.exception
+
+            if (not self.summariseFirst) and self.__correct_one_date(noticed):
+                # If the user chose to overwrite, go around again and refresh
+                # the file state.
+                continue
+            
+            return noticed
+
+    def __print_errors(self):
+        errorCount = 0
+        for noticed in self._noticedFiles:
+            if noticed.exception is None: continue
+            errorCount += 1
+            print(noticed.path)
+            print(f"Raised {type(noticed.exception).__name__}.")
+            if type(noticed.exception) is UnicodeDecodeError: print(
+                f"Should the suffix {noticed.path.suffix} be an exempt binary"
+                " format?")
+        return errorCount
+
+    def __print_summary(self):
+        formats = {}
+        noticeSuffixes = {}
+        for noticed in self._noticedFiles:
+            fileFormat = (
+                noticed.path.name if (
+                    noticed.path.name in self.exemptNames
+                    or noticed.path.suffix == ""
+                ) else noticed.path.suffix
+            )
+            if fileFormat not in formats: formats[fileFormat] = {}
+            if noticed.state.name not in formats[fileFormat]:
+                formats[fileFormat][noticed.state.name] = []
+            formats[fileFormat][noticed.state.name].append(noticed.path)
+
+            noticeSuffix = (
+                ascii(None) if (
+                    noticed.notice is None or noticed.notice.suffix is None
+                ) else noticed.notice.suffix)
+            if noticeSuffix not in noticeSuffixes:
+                noticeSuffixes[noticeSuffix] = []
+            noticeSuffixes[noticeSuffix].append(noticed.path)
+
         indent = " " * 4
-        for suffix in sorted(self._suffixes.keys()):
-            print(suffix)
-            summary = self._suffixes[suffix]
-            for state, count in summary.items():
-                print(''.join((indent, state, ": ", str(count), ".")))
+        for fileFormat in sorted(formats.keys()):
+            print(fileFormat)
+            states = formats[fileFormat]
+            for state in sorted(states.keys()):
+                print(''.join((
+                    indent, state, ": ", first_or_len(states[state]) )))
+        for noticeSuffix in sorted(noticeSuffixes.keys()):
+            print(
+                str_quote(None if noticeSuffix == ascii(None) else noticeSuffix)
+                , first_or_len(noticeSuffixes[noticeSuffix]))
+
+    def __correct_dates(self):
+        for noticed in self._noticedFiles:
+            self.__correct_one_date(noticed)
+
+    def __correct_one_date(self, noticedFile):
+        if noticedFile.state is not NoticeState.INCORRECT_DATE: return False
+        editedPath = noticedFile.notice.rewrite_year(
+            noticedFile.gitModifiedDate.year)
+        return self._overwrite.prompt(noticedFile.path, editedPath)
