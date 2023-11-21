e86192706884b4fc137d260efd22f9f63fe1c177
Jim Hawkins <hawkinsji@vmware.com>
2023-11-21 12:27:02 +0000

    Add in-progress changes to local Mac app for safekeeping
    Signed-off-by: Jim Hawkins <hawkinsji@vmware.com>

--- forApple/MacSkeleton/MacSkeleton/ViewController.swift
+++ forApple/MacSkeleton/MacSkeleton/ViewController.swift
@@ -6,33 +6,36 @@ import WebKit
 
 import CaptiveWebView
 
-class ViewController: NSViewController, CaptiveWebViewCommandHandler {
+class ViewController: NSViewController, CaptiveWebView.ViewController { // CaptiveWebViewCommandHandler {
+
+
     func handleCommand(
         _ command: Dictionary<String, Any>) -> Dictionary<String, Any>
     {
         // Dummy handler for now.
         return command
     }
-    
-    var webView: WKWebView!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        self.webView = CaptiveWebView.makeWebView(
-            frame: self.view.frame, commandHandler: self)
-        self.view.addSubview(self.webView, positioned: .below, relativeTo: nil)
-        CaptiveWebView.constrain(view:webView, to:view)
-
-        // Uncomment the following to add a diagnostic border around the web
-        // view.
-        // self.webView.layer?.borderColor = NSColor.blue.cgColor
-        // self.webView.layer?.borderWidth = 4.0
-        // self.webView.layer?.backgroundColor = NSColor.yellow.cgColor
-
-        _ = CaptiveWebView.load(
-            in: self.webView, scheme: CaptiveWebView.scheme, file: "Main.html")
-    }
+//    
+//    var webView: WKWebView!
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        self.webView = CaptiveWebView.makeWebView(
+//            frame: self.view.frame, commandHandler: self)
+//        self.view.addSubview(self.webView, positioned: .below, relativeTo: nil)
+//        CaptiveWebView.constrain(view:webView, to:view)
+//
+//        // Uncomment the following to add a diagnostic border around the web
+//        // view.
+//        // self.webView.layer?.borderColor = NSColor.blue.cgColor
+//        // self.webView.layer?.borderWidth = 4.0
+//        // self.webView.layer?.backgroundColor = NSColor.yellow.cgColor
+//
+//        _ = CaptiveWebView.load(
+//            in: self.webView, scheme: CaptiveWebView.scheme, file: "Main.html")
+//    }
 
     override var representedObject: Any? {
         didSet {
